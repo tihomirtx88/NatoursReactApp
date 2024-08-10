e00a7540e2b3193d1f36c1a8a5a2fc71dbc91c1c
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-10 21:29:59 +0300

    Create login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,40 @@
+const Login = () => {
+  return (
+    <main className="main">
+      <div className="login-form">
+        <h2 className="heading-secondary ma-bt-lg">Log into your account</h2>
+        <form className="form">
+          <div className="form__group">
+            <label htmlFor="email" className="form__label">
+              Email address
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="you@example.com"
+              required={true}
+              className="form__input"
+            />
+          </div>
+          <div className="form__group ma-bt-md">
+            <label htmlFor="password" className="form__label">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="••••••••"
+              required={true}
+              className="form__input"
+            />
+          </div>
+          <div className="form__group">
+            <button className="btn btn--green">Login</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
