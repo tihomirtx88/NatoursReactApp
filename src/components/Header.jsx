f4c03cedf6e97206c0c45acb9ec69f538ee106b4
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-11 09:37:21 +0300

    Refacturing part of code and add buttons to header nav for login and register

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ const Header = () => {
     <header className="header">
       <div className="header__logo-box">
         <img src="img/logo-white.png" alt="Logo" className="header__logo" />
+        <div className="header__container">
+          <a href="#" className="header__container--login">Login</a>
+          <a href="#" className="header__container--register">Register</a>
+
       </div>
+      </div>
+    
       <div className="header__text-box">
         <h1 className="heading-primary">
           <span className="heading-primary--main">Outdoors</span>
