f985afc74dc85774709db4ef20d4f65f59289df3
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-14 09:40:26 +0300

    Implement mapbox functionality for tour description page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,7 @@
-import About from "./About";
-
-import Features from "./Features";
-import Stories from "./Stories";
-import Tours from "./Tours";
-import Footer from "./Footer";
-import Popup from "./Popup";
-
-
 const Main = () => {
   return (
     <>
-    
-       <About />
-      <Features />
-      <Tours />
-      <Stories />
-      <Popup/> 
-      <Footer/>
+      <div>dsdsa</div>
     </>
   );
 };
