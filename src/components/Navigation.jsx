165deab7284b81af14b71347d67d1b6008c82708
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-18 18:37:38 +0300

    Refacturing header and navigation components and add functionality to close pop up affter choise montly year trips

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,46 +1,88 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+
+
 const Navigation = () => {
-    return(
-        <div className="navigation">
-        <input
-          type="checkbox"
-          className="navigation__checkbox"
-          id="navi-toogle"
-        />
-        <label htmlFor="navi-toogle" className="navigation__button">
-          <span className="navigation__icon">&nbsp;</span>
-        </label>
-        <div className="navigation__background">&nbsp;</div>
-        <nav className="navigation__nav">
-          <ul className="navigation__list">
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>01</span>About natours
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>02</span>Your benefits
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>03</span>Popular tours
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>04</span>Stories
-              </a>
-            </li>
-            <li className="navigation__item">
-              <a href="#" className="navigation__link">
-                <span>05</span>Book now
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
+  const navigate = useNavigate();
+  const currentYear = new Date().getFullYear();
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [isChecked, setIsChecked] = useState(false); // State for the checkbox
+
+  // Generate an array of the last ten years
+  const years = Array.from({ length: 10 }, (_, i) => currentYear - i);
+
+  const handleYearChange = (event) => {
+    const year = event.target.value;
+    setSelectedYear(year);
+    setTimeout(() => {
+      navigate(`/montly-plan/${year}`);
+    }, 0);
+  };
+
+  const handleNavigate = () => {
+    
+    setIsChecked(false);   // Uncheck the navigation checkbox
+    setTimeout(() => {
+      navigate(`/montly-plan/${selectedYear}`);
+    }, 0);
+  };
+
+  return (
+    <div className="navigation">
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+        checked={isChecked} // Set the checkbox state
+        onChange={() => setIsChecked(!isChecked)} // Toggle checkbox state
+      />
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">&nbsp;</span>
+      </label>
+      <div className="navigation__background">&nbsp;</div>
+      <nav className="navigation__nav">
+        <ul className="navigation__list">
+          <li className="navigation__item">
+            <Link onClick={handleNavigate} className="navigation__link">
+              <span>01</span>Check Monthly Plan Tours
+            </Link>
+            <select
+              value={selectedYear}
+              onChange={handleYearChange}
+              className="navigation__select"
+            >
+              {years.map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>02</span>Your benefits
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>03</span>Popular tours
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>04</span>Stories
+            </a>
+          </li>
+          <li className="navigation__item">
+            <a href="#" className="navigation__link">
+              <span>05</span>Book now
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,43 +1,49 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
+
 const Popup = () => {
   return (
     <div className="popup" id="popup">
-      <div className="popup__content">
-        <div className="popup__left">
-          <img src="./img/nat-8.jpg" alt="Tour photo" className="popup__img" />
-          <img src="./img/nat-9.jpg" alt="Tour photo" className="popup__img" />
-        </div>
-        <div className="popup__right">
-          {/* For can be section-tours target affter close  */}
-          <a href="#section-tours" className="popup__close">
-            ×
-          </a>
-          <h2 className="heading-secondary u-margin-bottom-medium">
-            start booking now
-          </h2>
-          <h3 className="heading-tertirary u-margin-bottom-small">
-            Importnant – Please read these terms before booking
-          </h3>
-          <p className="popup__text">
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum The Extremes of
-            Good and Evil by Cicero, written in 45 BC. This book is a treatise
-            on the theory of ethics, very popular during the Renaissance. The
-            first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from
-            a line in section 1.10.32. The standard chunk of Lorem Ipsum used
-            since the 1500s is reproduced below for those interested. Sections
-            1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are
-            also reproduced in their exact original form, accompanied by En
-          </p>
-          <a href="#" className="btn btn-green" />
-        </div>
+    <div className="popup__content">
+      <div className="popup__left">
+        <img src="./img/nat-8.jpg" alt="Tour photo" className="popup__img" />
+        <img src="./img/nat-9.jpg" alt="Tour photo" className="popup__img" />
+      </div>
+      <div className="popup__right">
+        <button className="popup__close" >
+          ×
+        </button>
+        <h2 className="heading-secondary u-margin-bottom-medium">
+          Start booking now
+        </h2>
+        <h3 className="heading-tertirary u-margin-bottom-small">
+          Important – Please read these terms before booking
+        </h3>
+        <p className="popup__text">
+          Contrary to popular belief, Lorem Ipsum is not simply random text.
+          It has roots in a piece of classical Latin literature from 45 BC,
+          making it over 2000 years old. Richard McClintock, a Latin professor
+          at Hampden-Sydney College in Virginia, looked up one of the more
+          obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+          going through the cites of the word in classical literature,
+          discovered the undoubtable source. Lorem Ipsum comes from sections
+          1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum The Extremes of
+          Good and Evil by Cicero, written in 45 BC. This book is a treatise
+          on the theory of ethics, very popular during the Renaissance. The
+          first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from
+          a line in section 1.10.32. The standard chunk of Lorem Ipsum used
+          since the 1500s is reproduced below for those interested. Sections
+          1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are
+          also reproduced in their exact original form, accompanied by En
+        </p>
+        {/* Use a Link or button for navigation */}
+        <Link to="#" className="btn btn-green">
+          Book now
+        </Link>
       </div>
     </div>
+  </div>
   );
 };
 
