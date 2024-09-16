f29e71d0ffd1d7f78c097958833c7209fc8b3700
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-09-16 19:43:44 +0300

    Creating and stilizited page not found

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MontlyPlanTours from "./features/tours/MontlyPlanTours";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { AuthProvider } from "./context/AuthContext";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   const queryClient = new QueryClient({
@@ -32,6 +33,7 @@ function App() {
           </Route>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
