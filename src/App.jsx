0930e5ef9fc67a7ddb338f75bd09d1a8cec82e8c
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-09-19 22:16:49 +0300

    Fix Toast visible functionality and create scroll top up functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./components/Dashboard";
 import TourDetailsPage from "./pages/tourDetailsPage";
@@ -37,6 +38,26 @@ function App() {
           <Route path="register" element={<RegisterPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+        <Toaster
+            position="top-right"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 5000,
+              },
+              style: {
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                backgroundColor: "white",
+                color: "var(--color-gray-700)",
+              },
+            }}
+          />
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
