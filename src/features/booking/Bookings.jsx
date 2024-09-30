d8334392d287653c413bcd3a18c518b50a166cac
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-09-30 21:28:58 +0300

    Functionality to filter booking by current user

--- src/features/booking/Bookings.jsx
+++ src/features/booking/Bookings.jsx
@@ -1,14 +1,21 @@
+import { useAuth } from "../../context/AuthContext";
 import BookingCard from "./BookingCard";
 import { useBookings } from "./useBookings";
 
 export default function Bookings(){
     const { bookings } = useBookings();
     const readingData = bookings?.data?.data || [];
+    const { user } = useAuth();
+
+    const userBookingsData = readingData.filter(booking => booking.user._id === user._id);
+    console.log(userBookingsData);
+    
+    
     
     return(
         <div className="card-container">
-      {readingData.length > 0 ? (
-        readingData.map((booking) => (
+      {userBookingsData.length > 0 ? (
+        userBookingsData.map((booking) => (
           <BookingCard key={booking._id} booking={booking} />
         ))
       ) : (
