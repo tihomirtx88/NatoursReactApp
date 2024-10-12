6c86038dc97ed134e54a65a454ede7e84e94a939
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-10-12 16:20:53 +0300

    Implementing updateTour and route also updateTour component with functionality for updating tour

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
-import Dashboard from "./components/Dashboard";
-import Book from "./features/booking/Book";
-import TourDetailsPage from "./pages/tourDetailsPage";
-import AppLayout from "./components/AppLayout";
-import Tours from "./features/tours/Tours";
-import MontlyPlanTours from "./features/tours/MontlyPlanTours";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import { AuthProvider } from "./context/AuthContext";
-import PageNotFound from "./pages/PageNotFound";
+// Pages
 import UserProfilePage from "./pages/UserProfilePage";
 import ResetPasswordPage from "./features/email/ResetPasswordPage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingsDetailsPage from "./pages/BookingDetailsPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TourDetailsPage from "./pages/tourDetailsPage";
+
+// Components
+import Dashboard from "./components/Dashboard";
+import Book from "./features/booking/Book";
+import AppLayout from "./components/AppLayout";
+import Tours from "./features/tours/Tours";
+import MontlyPlanTours from "./features/tours/MontlyPlanTours";
+import { AuthProvider } from "./context/AuthContext";
+import PageNotFound from "./pages/PageNotFound";
 import CreateTourForm from "./features/tours/CreateTourForm";
+import UpdateTourForm from "./features/tours/UpdateTourForm";
 
 function App() {
   const queryClient = new QueryClient({
@@ -37,6 +41,7 @@ function App() {
             <Route path="montly-plan/:year" element={<MontlyPlanTours />} />
             <Route path="tours" element={<Tours />} />
             <Route path="create/booking" element={<Book/>}/>
+            <Route path="updateTour/:tourId" element={<UpdateTourForm />} />
             <Route path="tours/:tourId" element={<TourDetailsPage />} />
             <Route path="profile" element={<UserProfilePage/>}/>
             <Route path="resetPassword/:token" element={<ResetPasswordPage/>}/>
