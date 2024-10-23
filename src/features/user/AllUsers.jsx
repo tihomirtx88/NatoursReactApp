0888ef400d96c534b39a16a733d79f7fce15c30d
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-10-23 08:01:21 +0300

    Implementing all users with dynamic data and adding sccs

--- src/features/user/AllUsers.jsx
+++ src/features/user/AllUsers.jsx
@@ -1,10 +1,53 @@
+import SpinnerMini from "../../components/SpinnerMini";
 import { useUsers } from "./useUsers";
+import styles from './AllUsers.module.scss';
 
-export default function AllUsers(){
-    const { allUsers, isLoadingUsers } = useUsers();
-    console.log(allUsers);
-    
-    return(
-        <div>users</div>
-    );
-};
+export default function AllUsers() {
+  const { allUsers, isLoadingUsers } = useUsers();
+
+  if (isLoadingUsers) {
+    return <SpinnerMini />;
+  }
+
+  const users = allUsers?.data?.users || [];
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.heading}>
+        <h1 className={styles['heading-primary']}>
+          <span className={styles['heading-primary--main']}>All Users</span>
+        </h1>
+      </div>
+
+      <div className={styles.container}>
+        {users.map(({ _id, name, email, photo, role }) => (
+          <div key={_id} className={styles.card}>
+            <div className={styles['card__header']}>
+              <img
+                className={styles['card__avatar']}
+                src={`http://localhost:3000/img/users/${photo}`}
+                alt={`${name} avatar`}
+                crossOrigin="anonymous"
+              />
+              <h2 className={styles['card__name']}>{name}</h2>
+            </div>
+
+            <div className={styles['card__details']}>
+              <p className={styles['card__detail']}>
+                <span>Email:</span> {email}
+              </p>
+              <p className={styles['card__detail']}>
+                <span>Role:</span> {role}
+              </p>
+            </div>
+
+            <div className={styles['card__actions']}>
+              <button className={`${styles.btn} ${styles['btn--update']}`}>Update</button>
+              <button className={`${styles.btn} ${styles['btn--delete']}`}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
